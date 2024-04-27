c66976c9ee4302947ab3b0aad017df8c1908b575
Marya <masha.smelkowa@yandex.ru>
2024-04-27 23:49:15 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int weight = 98; // вес в килограммах
-        double height = 1.87; // рост в метрах
+        int weight = 67; // вес в килограммах
+        double height = 1.70; // рост в метрах
         double bmi = service.calculate(weight, height); // считаем ИМТ
-        String bmiResult = String.format("%.1f",bmi); // округляем до 1-х знака
+        String bmiResult = String.format("%.0f",bmi); // округляем до целого числа
 
         System.out.println("Ваш вес: " + weight + " кг.");
         System.out.println("Ваш рост: " + height + " м.");
